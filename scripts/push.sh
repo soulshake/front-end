0d5aed4ea5b7af3f1ca3c97c07bc7e18ff93e7ae
AJ Bowen <aj@soulshake.net>
2020-10-16 19:08:11 +0200

    Replace master with main

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,30 +2,30 @@
 
 set -ev
 
-if [[ -z "$GROUP" ]] ; then
+if [[ -z "$GROUP" ]]; then
     echo "Cannot find GROUP env var"
     exit 1
 fi
 
-if [[ -z "$COMMIT" ]] ; then
+if [[ -z "$COMMIT" ]]; then
     echo "Cannot find COMMIT env var"
     exit 1
 fi
 
 push() {
-    DOCKER_PUSH=1;
-    while [ $DOCKER_PUSH -gt 0 ] ; do
-        echo "Pushing $1";
-        docker push $1;
-        DOCKER_PUSH=$(echo $?);
-        if [[ "$DOCKER_PUSH" -gt 0 ]] ; then
-            echo "Docker push failed with exit code $DOCKER_PUSH";
-        fi;
-    done;
+    DOCKER_PUSH=1
+    while [ $DOCKER_PUSH -gt 0 ]; do
+        echo "Pushing $1"
+        docker push $1
+        DOCKER_PUSH=$(echo $?)
+        if [[ "$DOCKER_PUSH" -gt 0 ]]; then
+            echo "Docker push failed with exit code $DOCKER_PUSH"
+        fi
+    done
 }
 
 tag_and_push_all() {
-    if [[ -z "$1" ]] ; then
+    if [[ -z "$1" ]]; then
         echo "Please pass the tag"
         exit 1
     else
@@ -35,16 +35,16 @@ tag_and_push_all() {
     if [[ "$COMMIT" != "$TAG" ]]; then
         docker tag ${DOCKER_REPO}:${COMMIT} ${DOCKER_REPO}:${TAG}
     fi
-    push "$DOCKER_REPO:$TAG";
+    push "$DOCKER_REPO:$TAG"
 }
 
-# Push snapshot when in master
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    tag_and_push_all master-${COMMIT:0:8}
-fi;
+# Push snapshot when in main
+if [ "$TRAVIS_BRANCH" == "main" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    tag_and_push_all main-${COMMIT:0:8}
+fi
 
 # Push tag and latest when tagged
 if [ -n "$TRAVIS_TAG" ]; then
     tag_and_push_all ${TRAVIS_TAG}
     tag_and_push_all latest
-fi;
+fi
